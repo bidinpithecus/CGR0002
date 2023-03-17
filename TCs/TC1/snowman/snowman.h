5f2bff462eed697c38c4adcfd88209c494542867
bidinpithecus <vinibidin@gmail.com>
2023-03-17 17:49:33 -0300

    README added and a folders structure

---
+++ TCs/TC1/snowman/snowman.h
@@ -0,0 +1,16 @@
+#ifndef _snowman_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glut.h>
+
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 600
+
+
+void display();
+void reshape(int, int);
+void keyboard(unsigned char, int, int);
+
+
+#endif
