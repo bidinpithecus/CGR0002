d3c7f1a34c579a65c0a202b406c7327267ac9c11
bidinpithecus <vinibidin@gmail.com>
2023-03-20 15:32:13 -0300

    Working on this bitchass snowie

--- TCs/TC1/snowman/snowman.h
+++ TCs/TC1/snowman/snowman.h
@@ -1,16 +1,16 @@
-#ifndef _snowman_h
+#ifndef _snowman_h_
+#define _snowman_h_
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <math.h>
 
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 600
 
-
 void display();
 void reshape(int, int);
 void keyboard(unsigned char, int, int);
-
 
 #endif
