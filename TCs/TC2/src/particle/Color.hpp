5806f4278641d2599db0cc8219a6996ded8275fa
bidinpithecus <vinibidin@gmail.com>
2023-04-04 10:09:08 -0300

    Color class implemented

--- TCs/TC2/src/particle/Color.hpp
+++ TCs/TC2/src/particle/Color.hpp
@@ -3,11 +3,16 @@
 
 class Color {
 	public:
+		Color();
 		Color(float r, float g, float b);
+		Color(unsigned char r, unsigned char g, unsigned char b);
 		Color(int hexColor);
 		float getRed();
 		float getGreen();
 		float getBlue();
+		void setRed(float r);
+		void setGreen(float g);
+		void setBlue(float b);
 
 	private:
 		float r;
